7041d1f13b3bd7a11a781be5b1ecaa90373c92cb
Sudeepseth44 <sudipseth44@gmail.com>
2024-12-15 16:14:14 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,8 @@
+const questions = [
+    {
+        question: "What is the powerhouse of the cell?",
+        options: ["Nucleus", "Mitochondria", "Ribosome", "Golgi Body"],
+        answer: "Mitochondria"
+    },
+    // Add more questions here
+];
